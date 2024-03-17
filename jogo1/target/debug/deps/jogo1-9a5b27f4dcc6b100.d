470a5502155eb5ebe2cc273a2b6c25ff93c62338
HTC-Informatica <hardtechcheaper@outlook.com>
2024-03-17 16:16:38 -0300

    começando

---
+++ jogo1/target/debug/deps/jogo1-9a5b27f4dcc6b100.d
@@ -0,0 +1,5 @@
+/data/data/com.termux/files/home/Rust/jogo1/target/debug/deps/jogo1-9a5b27f4dcc6b100: src/main.rs
+
+/data/data/com.termux/files/home/Rust/jogo1/target/debug/deps/jogo1-9a5b27f4dcc6b100.d: src/main.rs
+
+src/main.rs:
---
+++ jogo1/target/debug/jogo1.d
@@ -0,0 +1,1 @@
+/data/data/com.termux/files/home/Rust/jogo1/target/debug/jogo1: /data/data/com.termux/files/home/Rust/jogo1/src/main.rs
